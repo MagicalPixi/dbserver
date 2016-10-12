87e74da82a2e9c6a43bbe246d8c68b7ba72e99ca
guoshencheng <648772021@qq.com>
2016-10-12 13:49:44 +0800

    add pixi user

--- routes/pixi.js
+++ routes/pixi.js
@@ -3,5 +3,6 @@ var common = require('../lib/db').pixi
 var sprite = {key: 'sid', name: 'sprite'}
 var source = {key: 'sid', name: 'source'}
 var greateSprite = {key: 'gsid', name: 'greateSprite'}
-kvs = [sprite, source, greateSprite]
+var user = {key: 'uid', name: 'user'}
+kvs = [sprite, source, greateSprite, user]
 module.exports = BaseDBRouter(kvs, common)
