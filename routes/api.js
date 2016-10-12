b5756041e782159516919fce2ad2fb00ef94d500
guoshencheng <648772021@qq.com>
2016-10-12 12:44:10 +0800

    add relation

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 var BaseDBRouter = require('./BaseDBRouter')
 var common = require('../lib/db').common
 var game = {key: 'gid', name: 'game'}
-kvs = [game]
+var user = {key: 'uid', name: 'user'}
+kvs = [game, user]
 module.exports = BaseDBRouter(kvs, common)
