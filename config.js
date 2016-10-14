ed12ba8c1d9811dba15b4d404efaa222558e0e1f
guoshencheng <648772021@qq.com>
2016-10-14 23:31:34 +0800

    change files to file

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ var dburl = env == 'production' ? 'mongodb://localhost/' : 'mongodb://localhost/
 var qiniu = {
   accessKey: "EyEwm6Bjadr4ojSFxpKWt6k-PoyT99D5l_qMCEaL",
   secretKey: "xOUHlBygVg_dIxPcgWmEVu7GG5jl_XVQ57mrV7o0",
-  bucket: 'magicalpixi',
-  domin: 'http://qiniu.magicalpixi.com/'
+  bucket: env == 'production' ? 'magicalpixi' : 'test',
+  domin: env == 'production' ? 'http://qiniu.magicalpixi.com/' : 'http://test.qiniu.magicalpixi.com/'
 }
 module.exports = {
   dburl, qiniu
